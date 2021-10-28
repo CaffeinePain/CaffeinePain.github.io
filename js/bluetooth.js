1b37db597a376fa07a6e3b282136d99fda7b9dd5
CaffeinePain <whwowns1021@gmail.com>
2021-10-28 18:26:33 +0900

    1

---
+++ js/bluetooth.js
@@ -0,0 +1,9 @@
+navigator.bluetooth.requestDevice({ acceptAllDevices: true, })
+  .then(device => {
+    // Human-readable name of the device.
+    console.log(device.name);
+
+    // Attempts to connect to remote GATT Server.
+    return device.gatt.connect();
+  })
+  .catch(error => { console.log(error); });
