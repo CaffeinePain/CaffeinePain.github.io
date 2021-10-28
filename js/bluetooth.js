af9c76af23f0352aa1419811728c62321f595461
CaffeinePain <whwowns1021@gmail.com>
2021-10-28 19:40:47 +0900

    Add Audio test

--- js/bluetooth.js
+++ js/bluetooth.js
@@ -1,9 +1,24 @@
-navigator.bluetooth.requestDevice({ acceptAllDevices: true, })
-  .then(device => {
-    // Human-readable name of the device.
-    console.log(device.name);
+const bluetoothBtn = document.getElementById("bluetooth");
+const audioBtn = document.getElementById("audio");
+const audio1 = new Audio("칼리 모닝콜.mp3");
 
-    // Attempts to connect to remote GATT Server.
-    return device.gatt.connect();
-  })
-  .catch(error => { console.log(error); });
+function bluetoothConnect() {
+    navigator.bluetooth.requestDevice({ acceptAllDevices: true, })
+    .then(device => {
+      // Human-readable name of the device.
+      console.log(device.name);
+  
+      // Attempts to connect to remote GATT Server.
+      return device.gatt.connect();
+    })
+    .then(server => { playAudio(); })
+    .catch(error => { console.log(error); });
+}
+
+function playAudio() {
+    audio1.play();
+}
+
+bluetooth.addEventListener("click", bluetoothConnect);
+
+audioBtn.addEventListener("click", playAudio);
